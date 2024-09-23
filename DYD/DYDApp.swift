d7ac51f93388a5cf7f46c7e45844dc6ab5ec88b2
CRooi <107915149+CRooi@users.noreply.github.com>
2024-09-23 15:13:21 +0800

    Initial Commit

---
+++ DYD/DYDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DYDApp.swift
+//  DYD
+//
+//  Created by CRooi on 2024/9/23.
+//
+
+import SwiftUI
+
+@main
+struct DYDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
